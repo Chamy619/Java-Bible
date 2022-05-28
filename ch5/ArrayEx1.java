0a9b6f65035870210ef4b48751c1c75133447750
Chamy619 <yd95069506@naver.com>
2022-05-28 14:29:26 +0900

    :sparkles: 5장 책 예제 풀이

---
+++ ch5/ArrayEx1.java
@@ -0,0 +1,21 @@
+class ArrayEx1 {
+    public static void main(String[] args) {
+        int[] score = new int [5];
+        int k = 1;
+
+        score[0] = 50;
+        score[1] = 60;
+        score[k + 1] = 70;
+        score[3] = 80;
+        score[4] = 90;
+
+        int temp = score[k + 2] + score[4];
+
+        for (int i = 0; i < score.length; i++) {
+            System.out.printf("score[%d]:%d%n", i, score[i]);
+        }
+
+        System.out.printf("temp:%d%n", temp);
+        System.out.printf("score[%d]:%d%n", 7, score[7]);
+    }
+}
